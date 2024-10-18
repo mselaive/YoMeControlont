e149c29219bae64dde6e6d9ed4539f6b0af38ed1
Matias Selaive <mselaive@alumnos.uai.cl>
2024-10-18 20:10:13 -0300

    Formulario confirmar receta

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,20 +1,4 @@
-/*!
 
-=========================================================
-* Argon Dashboard React - v1.2.4
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2024 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import { useState } from "react";
 // node.js library that concatenates classes (strings)
 import classnames from "classnames";
@@ -49,17 +33,14 @@ import {
 import Header from "components/Headers/Header.js";
 
 const Index = (props) => {
-  const [activeNav, setActiveNav] = useState(1);
-  const [chartExample1Data, setChartExample1Data] = useState("data1");
-
+  
   if (window.Chart) {
     parseOptions(Chart, chartOptions());
   }
 
   const toggleNavs = (e, index) => {
     e.preventDefault();
-    setActiveNav(index);
-    setChartExample1Data("data" + index);
+
   };
   return (
     <>
@@ -75,7 +56,7 @@ const Index = (props) => {
                 <Col>
 
                     <div className="text-center text-muted mb-4">
-                        <div className="text-center font-weight-bold h2 mb-4">
+                        <div className="text-center font-weight-bold h1 mb-4">
                             <big>{'Formulario'}</big>
                         </div>
                     </div>
